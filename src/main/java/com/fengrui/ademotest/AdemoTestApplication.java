232a14bc036ecf146fd8d539e299d462056b83bf
赵洪 <2795015155@qq.com>
2026-06-09 16:32:05 +0800

    初始化

---
+++ src/main/java/com/fengrui/ademotest/AdemoTestApplication.java
@@ -0,0 +1,13 @@
+package com.fengrui.ademotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdemoTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdemoTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fengrui/ademotest/AdemoTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fengrui.ademotest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdemoTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
